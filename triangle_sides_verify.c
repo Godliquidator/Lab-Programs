e2eaef9b50af042be25d652e19b178c6dacd73ee
Archit Gupta <guptarchit2k17@gmail.com>
2021-11-04 02:53:08 +0530

    Updated triangle_sides_verify.c

--- triangle_sides_verify.c
+++ triangle_sides_verify.c
@@ -9,6 +9,6 @@ int main()
     printf("Enter the value of side C:\n");
     scanf("%d", &c);
     t = ((a + b) > c) && ((b + c) > a) && ((a + c) > b);
-    printf("Do these sides verify triangle property ? 1 for Yes, 0 for No\nThe value for provided side length is %d.", t);
+    printf("Do these sides verify triangle property ? 1 for Yes, 0 for No\nThe value for provided side length is %d.", t);  
     return 0;
 }
