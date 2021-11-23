0a55cef49e5b33f9b0216c3ad8511cc2bbd427d2
Archit Gupta <guptarchit2k17@gmail.com>
2021-11-24 02:37:40 +0530

    Created on 23.11.2021

--- prime.c
+++ prime.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int n, i, flag = 1;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
-    for (i = 2; i <= n / 2; ++i)
+    int t = n / 2;
+    for (i = 2; i <= t; ++i)
     {
         if (n % i == 0)
         {
-            flag = 1;
+            flag = 0;
             break;
         }
     }
@@ -20,7 +20,7 @@ int main()
     }
     else
     {
-        if (flag == 0)
+        if (flag)
             printf("%d is a prime number.", n);
         else
             printf("%d is not a prime number.", n);
---
+++ product_even_num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, p = 1, num;
+    printf("How many numbers you want to enter ? ");
+    scanf("%d", &n);
+    i = 1;
+    while (i <= n)
+    {
+        printf("Enter the number: ");
+        scanf("%d", &num);
+        if (num % 2 == 0)
+        {
+            p = p * num;
+        }
+        i++;
+    }
+    printf("The product of even numbers among following numbers is %d", p);
+    return 0;
+}
